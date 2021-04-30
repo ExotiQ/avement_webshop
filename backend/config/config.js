e10d44fe3989892ad932b74936f4eb02efc953c1
Flo Gesell <florian.gesell@hs-augsburg.de>
2021-04-30 12:59:18 +0200

    Initial commit

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,11 +1,12 @@
+
 const { POSTGRES_HOST, POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB } = process.env;
 
 module.exports = {
   "development": {
-    "username": POSTGRES_USER,
-    "password": POSTGRES_PASSWORD,
-    "database": POSTGRES_DB,
-    "host": POSTGRES_HOST,
+    "username": process.env.POSTGRES_USER,
+    "password": process.env.POSTGRES_PASSWORD,
+    "database": process.env.POSTGRES_DB,
+    "host": "0.0.0.0",
     "dialect": "postgres"
   },
   "test": {

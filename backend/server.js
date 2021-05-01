1ecccf39ff527c4548674b4c092b5df677d6c17f
Flo Gesell <florian.gesell@hs-augsburg.de>
2021-05-01 15:43:46 +0200

    added products model

--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,15 @@
 const express = require("express");
 const app = express();
 const db = require('./config/database');
-const User = require('./models/user')
 
 async function main() {
   const { PORT } = process.env;
 
-  // Test DB Authentication
   db.authenticate().then(() => console.log("Connected to database..."))
   await db.sync()
 
-  // await User.create({
-  //   firstName: "Florian",
-  //   lastName: "Gesell",
-  //   email: "florian@gesell.com"
-  // });
-
-  // await User.create({
-  // firstName: "Luis",
-  // lastName: "Schweigard",
-  // email: "luis@schweigard.com"
-  // });
-
-  console.log(JSON.stringify(await User.findAll(), null, 2))
-
-  const Items = require("./routes/Items");
-  app.use("/api/items", Items);
+  const Products = require("./routes/Products");
+  app.use("/api/products", Products);
 
   const Auth = require("./routes/Auth");
   app.use("/api/auth", Auth);

733592ee0d186a30c6b6a8abc2c381581a43acd7
Flo Gesell <florian.gesell@hs-augsburg.de>
2021-05-07 18:47:00 +0200

    removed signing admin

--- backend/routes/Auth.js
+++ backend/routes/Auth.js
@@ -2,8 +2,9 @@ const express = require("express");
 const auth = express.Router();
 const db = require('../config/database');
 const User = require('../models/user');
+
 const jwt = require('jsonwebtoken');
-const authenticateJWT = require('../middleware/authentification.js')
+const authentificate = require('../middleware/authentification.js')
 
 const { SECRET } = process.env;
 
@@ -14,13 +15,14 @@ async function main() {
   await db.sync()
 }
 
+// LOGIN
 auth.get('/login', async function (req, res) {
   const { username, password } = req.body;
   if(username !== "" && password !== "") {
     const account = await User.findOne({ where: { email: username } });
     if (account) {
       // Generate an access token
-      const accessToken = jwt.sign({ username: account.email,  role: account.admin }, SECRET);
+      const accessToken = jwt.sign({ username: account.email }, SECRET);
       res.json({
           accessToken
       });
@@ -30,6 +32,7 @@ auth.get('/login', async function (req, res) {
   }
 })
 
+// REGISTER
 auth.post('/register', async function (req, res) {
     if(req !== null) {
       const email = await User.findOne({ where: { email: req.body.email } });
@@ -47,8 +50,15 @@ auth.post('/register', async function (req, res) {
     }
 })
 
-auth.get('/list', authenticateJWT, async function (req, res) {
+// LIST ALL USER
+auth.get('/list', authentificate, async function (req, res) {
   res.status(200).json(await User.findAll(), null, 2);
+})
+
+auth.get('/test', authentificate, async function (req, res) {
+  const account = await User.findOne({ where: { email: req.user.username } });
+  
+  res.status(200).json();
 })
 
 module.exports = auth;

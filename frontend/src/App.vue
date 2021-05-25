0c9411d72ee9b679c540fba99c96e48036e7bab2
robing <robin.geramb@hs-augsburg.de>
2021-05-25 17:42:21 +0200

    start frontend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,27 +1,78 @@
 <template>
+  
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view />
+    
   </div>
 </template>
 
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+  
+  },
+}
+</script>
+
 <style lang="scss">
+
+.align-left{
+        float: left;
+    }
+
+.align-right{
+    float: right;
+}
+
+body{
+  margin: 0px;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Montserrat', serif;
+  font-weight: 500;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  width: 100%;
+  //background-color: mediumspringgreen;
+  margin: 0px;
+  padding: 0px;
+  
+}
+
+#background{
+  background-image: url("./assets/bg-image1.jpg");
+  background-position: center; /* Center the image */
+  background-repeat: no-repeat; /* Do not repeat the image */
+  background-size: cover; 
+  width: 100vw;
+  height: 100vh;
+}
+
+#center-block{
+  display: inline-block;
+  position: absolute;
+  width: 476px;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+#center-block p{
+  color: white;
+  margin-bottom: 0px;
+  margin-top: 0px;
+}
+
+#center-logo{
+  height: 96px;
 }
 
 #nav {
-  padding: 30px;
-
   a {
-    font-weight: bold;
     color: #2c3e50;
 
     &.router-link-exact-active {
---
+++ frontend/src/components/Sidenav.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <a href="/Shop/tops">TOPS</a>
+        <a href="/Shop/tops">BOTTOMS</a>
+        <a href="/Shop/tops">ACCESORIES</a>
+        <a style="color: red" href="/Shop/tops">SALE</a>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'Sidenav',
+        props:{
+
+        }
+    }
+</script>
+
+<style scoped>
+    div{
+        display: inline-block;
+        text-align: left;
+        width: 70px;
+        position: absolute;
+        left: 70px;
+        top: 50%;
+        transform: translate(0%, -50%);
+    }
+
+    a{
+        text-decoration: none;
+        display: inline-block;
+        color: black;
+        margin-bottom: 10px;
+        font-weight: 500;
+        font-size: 14px;
+    }
+</style>

b12f338962b64d9f71cb27046c23805cf7a4db3d
Karl Diedrichsen <kdiedrichsen@me.com>
2021-05-31 16:27:36 +0200

    new datamodel

---
+++ backend/models/e_product_variant.js
@@ -0,0 +1,26 @@
+const { Sequelize, Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+class e_product_variant extends Model {};
+
+e_product_variant.init({
+    id: {
+        type:           Sequelize.INTEGER,
+        primaryKey:     true,
+        autoIncrement:  true
+    },
+    p_id: {      
+        type:       Sequelize.INTEGER,
+        allowNull:  false,
+        references: {
+            model:  'e_product',
+            key:    'id'
+        }
+    },
+    size:       Sequelize.STRING(10),
+    color:      Sequelize.STRING(20),
+    images:     Sequelize.ARRAY(Sequelize.INTEGER),
+    quantity:   Sequelize.INTEGER
+}, { sequelize, modelName: "e_product_variant"});
+
+module.exports = e_product_variant;

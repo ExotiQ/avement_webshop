e64eccf5cd823382a596c5a003f4d014d267828e
Flo Gesell <fgesell@hotmail.de>
2021-06-16 18:12:12 +0200

    added migrations, models & changed order

---
+++ backend/migrations/3-create-e-product.js
@@ -0,0 +1,40 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('e_products', {
+      id: {
+        type:           Sequelize.INTEGER,
+        primaryKey:     true,
+        autoIncrement:  true
+      },
+      name: {
+          type:       Sequelize.STRING(50),
+          allowNull:  false
+      },
+      category: {   
+          type:       Sequelize.INTEGER,
+          allowNull:  false,
+          references: {
+              model:  'enum_categories',
+              key:    'id'
+          }
+      },
+      price: {
+          type: Sequelize.DECIMAL(7, 2),
+          allowNull: false
+      },
+      description:    Sequelize.TEXT,
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('e_products');
+  }
+};

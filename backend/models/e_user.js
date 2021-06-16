879fd44859159313220a6e87ec202a6e821efa44
Flo Gesell <fgesell@hotmail.de>
2021-06-16 17:56:31 +0200

    added primitive model

--- backend/models/e_user.js
+++ backend/models/e_user.js
@@ -1,63 +1,45 @@
-const uuid = require('uuid/v4');
+const { Sequelize, Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
 
-'use strict';
+class e_user extends Model {};
 
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class e_user extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  };
-  e_user.init({
-    id: {
-      allowNull: false,
-      primaryKey: true,
-      type: Sequelize.UUID
-    },
-    firstName: { 
-        type:       Sequelize.STRING(30),
-        allowNull:  false,
-        validate: {
-            is: ["^[a-z]+$",'i']
-        }
-    },
-    lastName: { 
-        type:       Sequelize.STRING(30),
-        allowNull:  false,
-        validate: {
-            is: ["^[a-z]+$",'i']
-        }
-    },
-    email: {       
-        type:       Sequelize.STRING(50),
-        unique:     true,
-        allowNull:  false,
-        validate: {
-            isEmail: true
-        }
-    },
-    isAdmin: {
-        type:           Sequelize.BOOLEAN,
-        allowNull:      false,
-        defaultValue:   false
-    },
-    password: {
-        type:       Sequelize.STRING(30),
-        allowNull:  false
-    }
-  }, {
-    sequelize,
-    modelName: 'e_user',
-  });
-  e_user.beforeCreate(user => user.id = uuid());
+e_user.init({
+  id: {
+    primaryKey: true,
+    type: DataTypes.UUID,
+    defaultValue: Sequelize.UUIDV4
+  },
+  firstName: { 
+      type:       Sequelize.STRING(30),
+      allowNull:  false,
+      validate: {
+          is: ["^[a-z]+$",'i']
+      }
+  },
+  lastName: { 
+      type:       Sequelize.STRING(30),
+      allowNull:  false,
+      validate: {
+          is: ["^[a-z]+$",'i']
+      }
+  },
+  email: {       
+      type:       Sequelize.STRING(50),
+      unique:     true,
+      allowNull:  false,
+      validate: {
+          isEmail: true
+      }
+  },
+  isAdmin: {
+      type:           Sequelize.BOOLEAN,
+      allowNull:      false,
+      defaultValue:   false
+  },
+  password: {
+      type:       Sequelize.STRING(30),
+      allowNull:  false
+  }
+}, { sequelize, modelName: 'e_user' });
 
-  return e_user;
-};
+module.exports = e_user;

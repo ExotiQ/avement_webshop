3f55dbde1c5d3d228fe1fb1c026ad544a71c6274
ExotiQ <exotiq@mad-gamers.net>
2021-06-17 18:07:51 +0200

    added sequelize.query

--- backend/routes/Products.js
+++ backend/routes/Products.js
@@ -3,6 +3,8 @@ const products = express.Router();
 const db = require('../config/database');
 const Products = require("../models/e_product");
 const User = require('../models/e_user');
+const { QueryTypes } = require('sequelize');
+const sequelize = require('sequelize');
 
 const authentificate = require('../middleware/authentification.js')
 
@@ -70,9 +72,16 @@ products.post('/remove/:id',authentificate, async function (req, res) {
       res.status(200).json("PRODUCT REMOVED: " + affectedRows);
     })
   }
-
-  
-  
 })
+
+products.get('/product_page/:name/:color', async function (req, res) {
+  const { name, color } = req.params;
+  const product_page = await Products.sequelize.query(
+  `SELECT p.name AS name, p_v.color AS color, p.price AS price, array_agg(e_image.image) AS image, array_agg(p_v.size, p_v.quantity) AS stock FROM  e_product p JOIN e_product_variant p_v ON p.id = p_v.p_id JOIN r_product_image p_i ON p_v.id = p_i.p_id JOIN e_image im ON p_i.i_id = im.id WHERE p.name = ${name} AND p_v.color = ${color} GROUP BY p.name, p_v.color, p.price`,
+  { type: QueryTypes.SELECT })
+
+  if(product_page !== null) res.status(200).json(product_page);
+  else res.status(400).json("NO SUCH PRODUCT PAGE");
+});
 
 module.exports = products;

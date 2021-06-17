80d80d03959fb04e1c26275c0beac6212145515e
ExotiQ <exotiq@mad-gamers.net>
2021-06-17 17:42:20 +0200

    added routes

--- backend/routes/Admin.js
+++ backend/routes/Admin.js
@@ -3,6 +3,9 @@ const admin = express.Router();
 const db = require('../config/database');
 const User = require('../models/e_user')
 const Image = require('../models/e_image')
+const Product = require('../models/e_product')
+const Variant = require('../models/e_product_variant')
+const Category = require('../models/enum_category')
 
 const jwt = require('jsonwebtoken');
 const authentificate = require('../middleware/authentification.js')
@@ -16,8 +19,60 @@ async function main() {
   await db.sync()
 }
 
+// ADD PRODUCT
+admin.post('/product/add', authentificate, async function (req, res) {
+  const { name,category,price,description } = req.body;
+  const account = await User.findOne({ where: { id: req.user.id } });
+
+  if(account.isAdmin === true) {
+      await Product.create({
+        name: name,
+        category: category,
+        price: price,
+        description: description,
+      }).then(function(product) {
+        res.status(200).json("PRODUCT ADDED");
+      }).catch(function (err) {
+        res.status(400).json(err);
+      });
+  }
+});
+
+// ADD PRODUCT CATEGORY
+admin.post('/category/add', authentificate, async function (req, res) {
+  const { name } = req.body;
+  const account = await User.findOne({ where: { id: req.user.id } });
+
+  if(account.isAdmin === true) {
+    await Category.create({
+      name: name
+    }) 
+    .then( () => {
+      res.status(200).json("CATEGORY ADDED");
+    })
+  }
+});
+
+// ADD PRODUCT VARIANT
+admin.post('/product/variant/add', authentificate, async function (req, res) {
+  const { p_id, size, color, quantity } = req.body;
+  const account = await User.findOne({ where: { id: req.user.id } });
+
+  if(account.isAdmin === true) {
+    await Variant.create({
+      p_id: p_id,
+      size: size,
+      color: color,
+      quantity: quantity
+    }) 
+    .then( () => {
+      res.status(200).json("VARIANT ADDED");
+    })
+  }
+});
+
 // ADD IMAGE
-admin.post('/all', authentificate, async function (req, res) {
+admin.post('/image/add', authentificate, async function (req, res) {
     const { name } = req.body;
 
 });

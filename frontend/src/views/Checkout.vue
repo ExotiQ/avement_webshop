04771f07355b984d499ac0dfd4644aa3d5c5c721
Flo <fgesell@hotmail.de>
2021-07-11 19:47:57 +0200

    added checkoutscreen

--- frontend/src/views/Checkout.vue
+++ frontend/src/views/Checkout.vue
@@ -4,12 +4,42 @@
     <div class="container">
       <div class="checkout">
         <form>
-          <input type="email" v-model="account.email" placeholder="email" />
+          <input type="email" v-model="order_details.email" placeholder="E-Mail Adresse" />
           <input
-            type="password"
-            v-model="account.password"
-            placeholder="Passwort"
-          />
+            type="text"
+            v-model="order_details.firstName"
+            placeholder="Vorname"
+          /> 
+          <input
+            type="text"
+            v-model="order_details.lastName"
+            placeholder="Nachname"
+          /> 
+          <input
+            type="text"
+            v-model="order_details.street"
+            placeholder="Straße und Hausnummer"
+          /> 
+          <input
+            type="text"
+            v-model="order_details.extraAdress"
+            placeholder="Zusätzliche Adressangaben"
+          /> 
+          <input
+            type="text"
+            v-model="order_details.zip"
+            placeholder="Postleitzahl"
+          /> 
+          <input
+            type="text"
+            v-model="order_details.city"
+            placeholder="Stadt"
+          /> 
+          <input
+            type="text"
+            v-model="order_details.country"
+            placeholder="Land"
+          /> 
           <div class="flex-container">
             <button class="package" @click="checkout">
               Weiter zum Versand
@@ -17,30 +47,91 @@
           </div>
         </form>
       </div>
-      <div class="warenkorb"></div>
+      <div class="warenkorb">
+        <div class="cart_block">
+        <Cartitem
+          v-for="(item, index) in cart"
+          :key="'item' + index"
+          :item="item.product"
+          :object="item"
+          :quantity="item.quantity"
+          :selectedSize="item.selectedSize"
+        />
+      </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import Header from "../components/Header";
+import Cartitem from "../components/CartItem.vue";
 
 export default {
   name: "Checkout",
   components: {
     Header,
+    Cartitem
   },
   data() {
     return {
-      account: {
+      order_details: {
         email: "",
-        password: "",
+        firstName: "",
+        lastName: "",
+        street: "",
+        extraAdress: "",
+        zip: "",
+        city: "",
+        country: ""
       },
     };
   },
+  computed: {
+    cart() {
+      return this.$store.getters.cart;
+    },
+    order() {
+      let products = [];
+
+      for (let index = 0; index < this.cart.length; index++) {
+        const element = this.cart[index];
+        const product = {
+          "p_v_id": 1,
+          "name":element.product.name,
+          "color": element.product.color,
+          "price": element.product.price,
+          "quantity": element.quantity
+        }
+        products.push(product);
+      }
+      return products;
+    }
+  },
   methods: {
+    async checkout(e) {
+      e.preventDefault();
+      console.log(this.order)
+
+      await this.$store.dispatch("checkout", this.order);
+
+    },
   },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.container {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  
+}
+
+.container form {
+  display: flex;
+  flex-direction: column;
+}
+
+</style>
